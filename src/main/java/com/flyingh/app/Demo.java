7512f29a622750500d1dabe8a2b17ae522392918
flyingher <springsource@yeah.net>
2012-11-01 21:47:26 +0800

    add some tests.

--- src/main/java/com/flyingh/app/Demo.java
+++ src/main/java/com/flyingh/app/Demo.java
@@ -2,9 +2,25 @@ package com.flyingh.app;
 
 import junit.framework.Assert;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 public class Demo {
+	@Test
+	public void test3(){
+		System.out.println(StringUtils.strip("a a b c \t"));
+		System.out.println(StringUtils.strip("a a b c \t", " "));
+		System.out.println(StringUtils.stripStart("abcde", "a"));
+		System.out.println(StringUtils.stripStart("aaabcde", "a"));
+		System.out.println(StringUtils.stripStart("aaabcde", "adcb"));
+	}
+	
+	@Test
+	public void test2() {
+		Assert.assertTrue(StringUtils.isBlank("\t"));
+		Assert.assertTrue(Character.isWhitespace('\t'));
+	}
+
 	@Test
 	public void test() {
 		Assert.assertTrue('\t' < ' ');
